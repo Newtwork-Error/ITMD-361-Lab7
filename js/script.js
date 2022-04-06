48911d58ec9df09712270dd484f5f1901fd2a844
Newtwork-Error <77990528+Newtwork-Error@users.noreply.github.com>
2022-04-05 21:21:10 -0500

    Added the javascript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+  function alertFunction(){
+    alert('Isaac Kane: ' + inputForm.value);
+  }
 
+  function setText(){
+    var outputText = document.getElementById('textoutput');
+    outputText.innerHTML = "<h2>" + inputForm.value + "</h2>";
+  }
 
-
-
-
-
-
+  var alertDiv = document.getElementById('entrybutton');
+  var inputForm = document.getElementById('entryinput');
+  alertDiv.addEventListener('click', alertFunction);
+  alertDiv.addEventListener('click', setText);
+}
 window.addEventListener('load', init);
